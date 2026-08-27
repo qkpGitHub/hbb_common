b394016276b52390c43136dca62a3291053da83f
qkpGitHub <54012802+qkpGitHub@users.noreply.github.com>
2026-08-27 11:47:17 +0800

    Configure default RustDesk servers

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &[`n    "140.206.117.2",`n    "103.164.32.252",`n    "120.204.248.178",`n];
+pub const RS_PUB_KEY: &str = "hDtUdMFZr3XtZV43zIqSwc2OWJHglFVMwQEyBsFjFMw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
